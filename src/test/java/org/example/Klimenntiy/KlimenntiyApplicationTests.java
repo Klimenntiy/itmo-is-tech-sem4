1fe3cdefb4deb3f238c7ca57b6ea9abc6f12c4c3
Klimenntiy <animilk04@gmail.com>
2025-04-19 02:15:34 +0300

    add lab-3

---
+++ src/test/java/org/example/Klimenntiy/KlimenntiyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.Klimenntiy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlimenntiyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
